b48337328f69d1c545c43f685590b636c3640e9f
valerieyang00 <valerieyang00@gmail.com>
2022-10-26 11:54:49 -0700

    initial axios set up for destinations main page

--- src/components/pages/Destinations.jsx
+++ src/components/pages/Destinations.jsx
@@ -1,7 +1,47 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+import { Link } from 'react-router-dom'
+
 export default function Destinations() {
+
+    const [msg, setMsg] = useState("")
+    const [destinations, setDestinations] = useState([])
+
+    useEffect(() => {
+
+        const getDestinations = async () => {
+            try {
+                const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api/destinations/`)
+                setDestinations(response.data)
+
+
+            }catch(err) {
+                console.warn(err)
+                if(err.response) {
+                    setMsg(err.response.data.msg)
+                }
+            }
+        }
+        getDestinations()
+    },[])
+
+    const renderPage = destinations.map((destination) => {
+        return (
+            <ul key={destination.id}>
+                <Link to={`/destinations/${destination.id}}`}><h2>{destination.name}</h2></Link>
+                <small>{destination.user}</small>
+                <img src={destination.photo} alt={destination.name} />
+            </ul>
+        )
+
+    })
+
     return (
+
         <div>
-            Destinations
+            {msg}
+            {renderPage}
         </div>
     )
 }
+
