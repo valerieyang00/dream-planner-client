9a6c10a74b02339446f633ad55448a380e3bf3b5
devin-lynch <devin.l.lynch@gmail.com>
2022-10-26 13:43:32 -0500

    Update class syntax on home.jsx

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -8,13 +8,13 @@ export default function Home() {
     return (
         <div>
             Home Page 🏡      
-            <div id="carouselExampleIndicators" class="carousel slide relative" data-bs-ride="carousel">
-                <div class="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
+            <div id="carouselExampleIndicators" className="carousel slide flex-auto" data-bs-ride="carousel">
+                <div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
                     <button
                     type="button"
                     data-bs-target="#carouselExampleIndicators"
                     data-bs-slide-to="0"
-                    class="active"
+                    className="active"
                     aria-current="true"
                     aria-label="Slide 1"
                     ></button>
@@ -31,46 +31,46 @@ export default function Home() {
                     aria-label="Slide 3"
                     ></button>
                 </div>
-                <div class="carousel-inner relative w-full overflow-hidden">
-                    <div class="carousel-item active  w-full">
+                <div className="carousel-inner relative h-[50vh] ">
+                    <div className="carousel-item active h-[50vh]">
                     <img
                         src="https://www.savoredjourneys.com/wp-content/uploads/2017/03/osaka-castle.jpg"
-                        class="flex w-45 h-45"
-                        alt="Wild Landscape"
+                        className="object-cover w-45 h-45"
+                        alt=""
                     />
                     </div>
-                    <div class="carousel-item  w-full">
+                    <div className="carousel-item h-[50vh]">
                     <img
                         src="https://www.frommers.com/system/media_items/attachments/000/856/538/s980/Paros2-crop.jpg?1464917118"
-                        class="flex w-45 h-45"
-                        alt="Camera"
+                        className="object-cover w-45 h-45"
+                        alt=""
                     />
                     </div>
-                    <div class="carousel-item  w-full">
+                    <div className="carousel-item h-[50vh]">
                     <img
                         src="https://travelpassionate.com/wp-content/uploads/2019/03/Scenic-summer-view-of-the-Old-Town-architecture-with-Elbe-river-embankment-in-Dresden-Saxony-Germany-Image-min-800x534.jpg"
-                        class="flex w-45 h-45"
-                        alt="Exotic Fruits"
+                        className="object-cover w-45 h-45"
+                        alt=""
                     />
                     </div>
                 </div>
                 <button
-                    class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+                    className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
                     type="button"
                     data-bs-target="#carouselExampleIndicators"
                     data-bs-slide="prev"
                 >
-                    <span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
+                    <span className="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
                 </button>
                 <button
-                    class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+                    className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
                     type="button"
                     data-bs-target="#carouselExampleIndicators"
                     data-bs-slide="next"
                 >
-                    <span class="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
+                    <span className="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
                 </button>
             </div>
         </div>
