b63b6e437cc983caae0cdfaae9036cc3307a3b4c
devin-lynch <devin.l.lynch@gmail.com>
2022-10-27 14:19:22 -0500

    Add links to github/linkedin

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,7 +1,46 @@
 export default function About() {
     return (
         <div>
-            About
+            About the devs
+            <div>
+                <h3>Valerie</h3>
+                <a href='https://github.com/valerieyang00' target='_blank'>
+                    <img src='https://i.imgur.com/Hmzo2zq.png' className='about' alt='github icon'/>
+                </a>
+                <a href='https://www.linkedin.com/in/valeriey/' target='_blank'>
+                    <img src='https://i.imgur.com/ssvYmKe.png' className='about' alt='linkedin icon'/>
+                </a>
+            </div>
+
+            <div>
+                <h3>Sean</h3>
+                <a href='https://github.com/mousesaver' target='_blank'>
+                    <img src='https://i.imgur.com/Hmzo2zq.png' className='about' alt='github icon'/>
+                </a>
+                <a href='https://www.linkedin.com/in/sean-f45/' target='_blank'>
+                    <img src='https://i.imgur.com/ssvYmKe.png' className='about' alt='linkedin icon'/>
+                </a>
+            </div>
+
+            <div>
+                <h3>Prija</h3>
+                <a href='https://github.com/prijacash' target='_blank'>
+                    <img src='https://i.imgur.com/Hmzo2zq.png' className='about' alt='github icon'/>
+                </a>
+                <a href='https://www.linkedin.com/in/prijacash/' target='_blank'>
+                    <img src='https://i.imgur.com/ssvYmKe.png' className='about' alt='linkedin icon'/>
+                </a>
+            </div>
+            
+            <div>
+                <h3>Devin</h3>
+                <a href='https://github.com/devin-lynch' target='_blank'>
+                    <img src='https://i.imgur.com/Hmzo2zq.png' className='about' alt='github icon'/>
+                </a>
+                <a href='https://www.linkedin.com/in/lynch-devin/' target='_blank'>
+                    <img src='https://i.imgur.com/ssvYmKe.png' className='about' alt='linkedin icon'/>
+                </a>
+            </div>
         </div>
     )
 }
