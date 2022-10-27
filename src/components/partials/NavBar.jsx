7a247757b336138283258d5ca36ac6fdd52e9c99
valerieyang00 <valerieyang00@gmail.com>
2022-10-27 16:15:50 -0700

    mydestination

--- src/components/partials/NavBar.jsx
+++ src/components/partials/NavBar.jsx
@@ -45,15 +45,15 @@ export default function Navbar({ currentUser, handleLogout }) {
 	return (
 		<nav className="relative top-0 flex justify-between items-end w-screen h-20 max-w-[90%] bg-slate-300 text-xl font-light text-stone-50 px-0 py-2">
                 {/* user always sees this section */}
-            <div className="flex items-end gap-3">
+            <div className="">
                 <Link to="/">Home</Link>{" | "}
-
-                <div className=''>
+{/* 
+                <div className=''> */}
                 <Link to='/destinations' className=''> Browse Destinations</Link>{" | "}
 
                 <Link to='/about' className=''>About</Link>
 
-                </div>
+                {/* </div> */}
             </div>
         {currentUser ? loggedIn : loggedOut}
 
