e2b76a76d59811466ce79aa6aab86698ce5baee5
prijacash <prijacash@gmail.com>
2022-10-26 19:56:37 -0700

    edit expense updated

--- src/components/pages/EditExpense.jsx
+++ src/components/pages/EditExpense.jsx
@@ -1,7 +1,120 @@
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+
+
 export default function EditExpense() {
+    const [msg, setMsg] = useState("")
+    const [form, setForm] = useState({
+        user: 1,
+        destination: 1,
+        date: '',
+        category: '',
+        merchant: '',
+        amount: 0,
+        details: ''
+
+
+    })
+
+    const { expenseId } = useParams()
+    const navigate = useNavigate()
+
+    const handleChange = (e) => {
+        setForm({...form, [e.target.name] : e.target.value})
+    }
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            await axios.post(`${process.env.REACT_APP_SERVER_URL}/api/expenses/${expenseId}`, form)
+            navigate('/destination/:id')
+        }catch(err){
+            console.warn(err)
+            if(err.response) {
+                setMsg(err.response.data.msg)
+            }
+        }
+    }
+
     return (
         <div>
-            Edit Expense         
+            <h1>Add expenses for your dream destination</h1>
+            <div className="w-full max-w-xs object-center">
+                <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <div className="mb-4">
+                        <label htmlFor="date" className="block text-gray-700 text-sm font-bold mb-2">Date:</label>
+                        <input 
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        type="date"
+                        name="date"
+                        id="date"
+                        placeholder="Expense date..."
+                        value={form.date}
+                        onChange={handleChange}
+                        required
+                        />
+                    </div>
+                    
+                    <div className="mb-4">
+                        <label htmlFor="merchant" className="block text-gray-700 text-sm font-bold mb-2">Merchant:</label>
+                        <input 
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        type="text"
+                        name="merchant"
+                        id="merchant"
+                        placeholder="Enter merchant..."
+                        value={form.merchant}
+                        onChange={handleChange}
+                        required
+                        />
+                    </div>
+
+                    <div className="mb-4">
+                        <label htmlFor="amount" className="block text-gray-700 text-sm font-bold mb-2">Amount:</label>
+                        <input 
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        type="number"
+                        name="amount"
+                        id="amount"
+                        placeholder="Enter amount..."
+                        value={form.amount}
+                        onChange={handleChange}
+                        required
+                        />
+                    </div>
+
+                    <div className="mb-6">
+                        <label htmlFor="details" className="block text-gray-700 text-sm font-bold mb-2">Details:</label>
+                        <input 
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        type="text"
+                        name="details"
+                        id="details"
+                        placeholder="Enter details..."
+                        value={form.details}
+                        onChange={handleChange}
+                        required
+                        />
+                    </div>
+
+                    <div className="mb-4">
+                        <label htmlFor='category'>Select Category:</label>
+                        <select value={form.category} name="category" onChange={handleChange}>
+                                <option value="lodging">Lodging</option>
+                                <option value="transportation">Transportation</option>
+                                <option value="food">Food</option>
+                                <option value="activities">Activities</option>
+                                <option value="misc">Misc</option>
+                        </select>
+                        <br></br>
+                        <br></br>
+                    </div>
+
+                    <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Submit</button>
+                </form>
+            </div>
         </div>
     )
 }
