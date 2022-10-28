85deea5de799d396b609763400845b21c7cdf43f
mousesaver <sean.fan87@gmail.com>
2022-10-28 13:54:03 -0700

    updated carousel

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -6,13 +6,13 @@ export default function Home() {
 
 
     return (
-        <div>
-                
-            <div id="carouselExampleIndicators" className="carousel slide flex my-20 justify-center" data-bs-ride="carousel">
-                <div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
+
+        <div class='flex justify-center'>
+            <div id="carouselExampleCaptions" className='carousel slide relative w-full max-w-[53rem] drop-shadow-lg'  data-bs-ride="carousel">
+                <div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-2">
                     <button
                     type="button"
-                    data-bs-target="#carouselExampleIndicators"
+                    data-bs-target="#carouselExampleCaptions"
                     data-bs-slide-to="0"
                     className="active"
                     aria-current="true"
@@ -20,44 +20,50 @@ export default function Home() {
                     ></button>
                     <button
                     type="button"
-                    data-bs-target="#carouselExampleIndicators"
+                    data-bs-target="#carouselExampleCaptions"
                     data-bs-slide-to="1"
                     aria-label="Slide 2"
                     ></button>
                     <button
                     type="button"
-                    data-bs-target="#carouselExampleIndicators"
+                    data-bs-target="#carouselExampleCaptions"
                     data-bs-slide-to="2"
                     aria-label="Slide 3"
                     ></button>
                 </div>
-                <div className="carousel-inner relative h-[50vh] ">
-                    <div className="carousel-item active h-[50vh]">
-                    <img
-                        src="https://www.savoredjourneys.com/wp-content/uploads/2017/03/osaka-castle.jpg"
-                        className="object-cover w-45 h-45"
-                        alt=""
-                    />
+
+                <div className="carousel-inner relative w-full overflow-hidden">
+                    <div className="carousel-item active relative float-left w-full">
+                        <img
+                            src="https://www.savoredjourneys.com/wp-content/uploads/2017/03/osaka-castle.jpg"
+                            // className="block w-full h-96 object-cover align-middle"
+                            className="aspect-video block w-full object-cover align-middle"
+                            alt="..."
+                        />
                     </div>
-                    <div className="carousel-item h-[50vh]">
-                    <img
-                        src="https://www.frommers.com/system/media_items/attachments/000/856/538/s980/Paros2-crop.jpg?1464917118"
-                        className="object-cover w-45 h-45"
-                        alt=""
-                    />
+
+                    <div className="carousel-item relative float-left w-full">
+                        <img
+                            src="https://www.savoredjourneys.com/wp-content/uploads/2017/03/osaka-castle.jpg"
+                            className="aspect-video block w-full object-cover align-middle"
+                            alt="..."
+                        />
                     </div>
-                    <div className="carousel-item h-[50vh]">
-                    <img
-                        src="https://travelpassionate.com/wp-content/uploads/2019/03/Scenic-summer-view-of-the-Old-Town-architecture-with-Elbe-river-embankment-in-Dresden-Saxony-Germany-Image-min-800x534.jpg"
-                        className="object-cover w-45 h-45"
-                        alt=""
-                    />
+
+                    <div className="carousel-item relative float-left w-full">
+                        <img
+                            src="https://travelpassionate.com/wp-content/uploads/2019/03/Scenic-summer-view-of-the-Old-Town-architecture-with-Elbe-river-embankment-in-Dresden-Saxony-Germany-Image-min-800x534.jpg"
+                            // className="block w-full h-96 object-cover align-middle"
+                            className="aspect-video block w-full object-cover align-middle"
+                            alt="..."
+                        />
                     </div>
                 </div>
+
                 <button
                     className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
                     type="button"
-                    data-bs-target="#carouselExampleIndicators"
+                    data-bs-target="#carouselExampleCaptions"
                     data-bs-slide="prev"
                 >
                     <span className="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
@@ -66,7 +72,7 @@ export default function Home() {
                 <button
                     className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
                     type="button"
-                    data-bs-target="#carouselExampleIndicators"
+                    data-bs-target="#carouselExampleCaptions"
                     data-bs-slide="next"
                 >
                     <span className="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
