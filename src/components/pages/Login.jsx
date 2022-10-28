2707d566910475cdbf3390f329aa07ba17881ca8
prijacash <prijacash@gmail.com>
2022-10-27 19:22:46 -0700

    tailwindd login

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -46,67 +46,72 @@ export default function Login({ currentUser, setCurrentUser }) {
 
 	return (
 		<div>
-			<p>{msg}</p>
-
 			<section className="h-screen">
-				<div className="container px-6 py-12 h-full">
-					<div className="">
-						<div className="">
-							<img
-							src="/loginNew.svg"
-							className="w-full"
-							
+				<div className="px-6 h-full text-gray-800">
+					<div
+					className="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6"
+					>
+					<div
+						className="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0"
+					>
+						<img
+						src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+						className="w-full"
+						alt="Sample image"
+						/>
+					</div>
+					<div className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
+						<form onSubmit={handleSubmit}>
+
+						{/* <!-- Email input --> */}
+						<div className="mb-6">
+							<input
+								type="text"
+								className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+								id="exampleFormControlInput2"
+								placeholder="Email address"
+								onChange={e => setEmail(e.target.value)}
+								value={email}
+								required
 							/>
 						</div>
 
-						<div className="">
-							<form onSubmit={handleSubmit}>
-								<p>Login to your Account:</p>
-								<br></br>
-
-								{/* <!-- Email input --> */}
-								<div className="mb-6 ">
-									<input 
-										type="email"
-										id="email"
-										className=""
-										placeholder="Email address"
-										onChange={e => setEmail(e.target.value)}
-										value={email}
-										required
-									/>
-								</div>
-
-								{/* <!-- Password input --> */}
-								<div className="mb-6">
-									<input 
-										type="password"
-										id="password"
-										className=""
-										placeholder="Password"
-										onChange={e => setPassword(e.target.value)}
-										value={password}
-										required
-									/>
-								</div>
-
-								{/* <!-- Submit button --> */}
-								<button
-									type="submit"
-									className=""
-									data-mdb-ripple="true"
-									data-mdb-ripple-color="light"
-								>
-									Sign in
-								</button>
-
-
-							</form>
+						{/* <!-- Password input --> */}
+						<div className="mb-6">
+							<input
+								type="password"
+								className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+								id="exampleFormControlInput2"
+								placeholder="Password"
+								onChange={e => setPassword(e.target.value)}
+								value={password}
+								required
+							/>
 						</div>
+
+						<div className="text-center lg:text-left">
+							<button
+								type="submit"
+								className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+								>
+								Login
+							</button>
+							<p className="text-sm font-semibold mt-2 pt-1 mb-0">
+							Don't have an account?
+							<a
+								href="#!"
+								className="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
+								>Register</a
+							>
+							</p>
+						</div>
+						</form>
+					</div>
 					</div>
 				</div>
 			</section>
-
+			
+			<p>{msg}</p>
 		</div>
 	)
 }
