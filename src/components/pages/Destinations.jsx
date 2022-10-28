8e8590b24e6a9efa7bc30b8e3dfcbd8398f067eb
devin-lynch <devin.l.lynch@gmail.com>
2022-10-28 13:06:24 -0500

    Add destinations cards

--- src/components/pages/Destinations.jsx
+++ src/components/pages/Destinations.jsx
@@ -31,16 +31,29 @@ export default function Destinations() {
         getDestinations()
     },[])
 
-    const renderPage = destinations.map((destination, idx) => {
-          return (
-            <ul key={`${destination.id}-${idx}`}>
-                <Link to={`/destinations/${destination.id}`}><h2 className='font'>{destination.name}</h2></Link>
-                <small className='font'>{destination.username}</small>
-                <Link to={`/destinations/${destination.id}`}><img src={destination.photo} alt={destination.name} className='rounded-3xl border-4 destinations' width='400px'/></Link>
-            </ul>
-        )
+    // const renderPage = destinations.map((destination, idx) => {
+    //       return (
+    //         <ul key={`${destination.id}-${idx}`}>
+    //             <Link to={`/destinations/${destination.id}`}><h2 className='font'>{destination.name}</h2></Link>
+    //             <small className='font'>{destination.username}</small>
+    //             <Link to={`/destinations/${destination.id}`}><img src={destination.photo} alt={destination.name} className='rounded-3xl border-4 destinations' width='400px'/></Link>
+    //         </ul>
+    //     )
 
-    })
+    // })
+
+    const renderPage = destinations.map((destination, idx) => {
+        return (
+          <div className="max-w-sm rounded overflow-hidden shadow-lg">
+              <ul key={`${destination.id}-${idx}`}>
+                  <Link to={`/destinations/${destination.id}`}><img src={destination.photo} alt={destination.name} className='rounded-3xl border-4 destinations' width='400px'/></Link>
+                  <Link to={`/destinations/${destination.id}`}><div className="font-bold text-xl mb-2">{destination.name}</div></Link>
+                  <small className="text-gray-700 text-base">{destination.username}</small>
+              </ul>
+          </div>
+      )
+
+  })
 
     return (
 
@@ -48,6 +61,11 @@ export default function Destinations() {
             {msg}
             {renderPage}
         </div>
+
+        
     )
 }
+
+
+
 
