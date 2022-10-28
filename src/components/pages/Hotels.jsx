69d5e33a9e6b85dcd3c521413f39af955c838203
valerieyang00 <valerieyang00@gmail.com>
2022-10-27 21:08:02 -0700

    hotels

---
+++ src/components/pages/Hotels.jsx
@@ -0,0 +1,36 @@
+import {useState, useEffect } from 'react';
+
+export default function Hotels() {
+
+    const [searchInput, setSearchInput] = useState("Barcelona")
+    const [search, setSearch] = useState(searchInput)
+
+    const handleSearch = (e) => {
+        setSearchInput(e.target.value)
+    }
+ 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setSearch(searchInput)
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="search">Search City:</label>
+                <input                 
+                type="text"
+                name="search"
+                id="search"
+                placeholder="Enter city name..."
+                value={searchInput}
+                onChange={handleSearch}
+                required
+                />
+                <button type='submit'>Submit</button>
+            </form>
+            <iframe 
+                src={`https://hatlastravel.com/widget/hotels?place=${search}&partner=1261`}
+                style={{height:"400px", width:"100%", margin:"auto", display:"block", overflow:"hidden", border:"none", marginTop:"10rem"}}></iframe> 
+        </div>
+    )
+}
