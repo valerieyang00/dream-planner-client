e0463e0bbbbf5142f4230f6056325efb2fd7bd0d
mousesaver <sean.fan87@gmail.com>
2022-10-29 15:41:27 -0700

    made home responsive

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,13 +1,14 @@
 import { useState, useEffect } from "react"
 import axios from "axios"
 import { Link } from 'react-router-dom'
+import Carousel from "../partials/Carousel"
 import 'tw-elements';
 
 export default function Home() {
 
     const [msg, setMsg] = useState("")
     const [destinations, setDestinations] = useState([])
-
+    const [windowSize, setWindowSize] = useState(window.innerWidth)
     useEffect(() => {
 
         const getDestinations = async () => {
@@ -32,6 +33,19 @@ export default function Home() {
         }
         getDestinations()
     },[])
+    useEffect(() => {
+        const handleResize = () => setWindowSize(window.innerWidth)
+  
+        window.addEventListener('resize', handleResize)
+  
+        return _ => window.removeEventListener('resize', handleResize)
+      }, [windowSize])
+  
+      const isWide = windowSize > 1536
+      const isThird = windowSize > 1280
+      const isHalf = windowSize > 1024
+      const isSmall = windowSize > 768
+      const isPhone = windowSize > 640
 
     // CARDS
     const renderPage = destinations.map((destination, idx) => {
@@ -48,92 +62,22 @@ export default function Home() {
     })   
 
     return (
-        <div>
-            <br></br>
-            <div className='flex justify-center'>
-                <div id="carouselExampleCaptions" className='carousel slide relative w-full w-7/12 drop-shadow-lg'  data-bs-ride="carousel">
-                    <div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-2">
-                        <button
-                            type="button"
-                            data-bs-target="#carouselExampleCaptions"
-                            data-bs-slide-to="0"
-                            className="active"
-                            aria-current="true"
-                            aria-label="Slide 1"
-                        ></button>
-                        <button
-                            type="button"
-                            data-bs-target="#carouselExampleCaptions"
-                            data-bs-slide-to="1"
-                            aria-label="Slide 2"
-                        ></button>
-                        <button
-                            type="button"
-                            data-bs-target="#carouselExampleCaptions"
-                            data-bs-slide-to="2"
-                            aria-label="Slide 3"
-                        ></button>
-                    </div>
-
-                    <div className="carousel-inner relative w-full overflow-hidden">
-                        <div className="carousel-item active relative float-left w-full">
-                            <img
-                                src="https://i.imgur.com/IqpIf8N.jpg"
-                                // className="block w-full h-96 object-cover align-middle"
-                                className="aspect-video block w-full object-cover align-middle"
-                                alt="..."
-                            />
-                        </div>
-
-                        <div className="carousel-item relative float-left w-full">
-                            <img
-                                src="https://i.imgur.com/pUY87ZV.png"
-                                className="aspect-video block w-full object-cover align-middle"
-                                alt="..."
-                            />
-                        </div>
-
-                        <div className="carousel-item relative float-left w-full">
-                            <img
-                                src="https://i.imgur.com/4dHKOHA.png"
-                                // className="block w-full h-96 object-cover align-middle"
-                                className="aspect-video block w-full object-cover align-middle"
-                                alt="..."
-                            />
-                        </div>
-                    </div>
-
-                    <button
-                        className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
-                        type="button"
-                        data-bs-target="#carouselExampleCaptions"
-                        data-bs-slide="prev"
-                    >
-                        <span className="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-                        <span className="visually-hidden">Previous</span>
-                    </button>
-                    <button
-                        className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
-                        type="button"
-                        data-bs-target="#carouselExampleCaptions"
-                        data-bs-slide="next"
-                    >
-                        <span className="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-                        <span className="visually-hidden">Next</span>
-                    </button>
-                </div>
-   
+        <div className={`relative flex flex-col overflow-x-hidden`}>
+            <div className={`flex ${!isWide ? 'flex-col-reverse' : ''} pt-16`}>
+              <div className={`relative w-screen ${isWide ? 'mt-40 self-end' : `mt-[-4rem] pb-[2rem] justify-center`} px-[${isThird && !isWide ? `mx-auto` : '0rem'}]`}>
+                <br></br>
+                <br></br>
+                <Carousel isWide={isWide} />
+              </div>
             </div>
-
             <div className="text-xl mt-5 ">
                 Most Popular Destinations
             </div>
-
             <div className="flex justify-center">
-                <div className="flex justify-center w-7/12 grid grid-cols-4 gap-4 mx-4 my-5 justify-items-center">
+                <div className={`flex justify-center w-7/12 grid ${isPhone ? 'grid-cols-4' : 'grid-cols-1'} gap-4 mx-4 my-5 justify-items-center`}>
                     {renderPage}
                 </div>
             </div>
-        </div>
+        </div>  
     )
 }
