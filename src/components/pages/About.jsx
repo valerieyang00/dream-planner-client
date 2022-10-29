f20d405f5503c68238ec57abc1d492c9d5a76c0c
devin-lynch <devin.l.lynch@gmail.com>
2022-10-29 16:58:27 -0500

    Update nightmode

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,10 +1,11 @@
 export default function About() {
     return (
         <div>
-            <section className="bg-white dark:bg-gray-900">
+            {/* <section className="bg-gray-50 dark:bg-gray-900">  need to add back to sections if we remove dark mode implementation */}
+            <section className=""> 
                 <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
                     <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
-                        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our Team</h2>
+                        <h2 className="mb-4 text-4xl tracking-tight font-extrabold ">Our Team</h2>
                         <p className="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">The Dream Planner App was created by this team <br></br> using Django, React, & PostgreSQL. </p>
                     </div> 
                     <div className="grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
